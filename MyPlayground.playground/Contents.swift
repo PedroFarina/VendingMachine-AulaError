cf86c18dc00a40358d1b541c703fdf05e74d80c6
Pedro Giuliano Farina <pedrogfarina@gmail.com>
2020-04-30 14:53:41 -0300

    Fazendo o exercício

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,30 +1,90 @@
 import Foundation
 
-struct VendingMachineProduct {
+class VendingMachineProduct {
     var name: String
     var amount: Int
     var price: Double
+
+    init(name: String, amount: Int, price: Double) {
+        self.name = name
+        self.amount = amount
+        self.price = price
+    }
+
+    func buy(with money: inout Double) {
+        money -= price
+        amount -= 1
+    }
+}
+
+enum VendingMachineError: Error {
+    case productNotFound
+    case productUnavailable
+    case insufficientFunds
+    case productStuck
 }
 
 //TODO: Definir os erros
 
 class VendingMachine {
     private var estoque: [VendingMachineProduct]
+    private var money: Double
     
     init(products: [VendingMachineProduct]) {
         self.estoque = products
+        self.money = 0
     }
     
     func getProduct(named name: String, with money: Double) throws {
         //TODO: receber o dinheiro e salvar em uma variável
+        self.money = money
         //TODO: achar o produto que o cliente quer
+        guard let product = estoque.first(where: {$0.name.lowercased() == name.lowercased()}) else { throw VendingMachineError.productNotFound }
         //TODO: ver se ainda tem esse produto
+        guard product.amount > 0 else { throw VendingMachineError.productUnavailable }
         //TODO: ver se o dinheiro é o suficiente pro produto
+        guard product.price <= self.money else { throw VendingMachineError.insufficientFunds }
         //TODO: entregar o produto
+        product.buy(with: &self.money)
+        if Int.random(in: 1...10) == 1 {
+            throw VendingMachineError.productStuck
+        }
     }
     
     func getTroco() -> Double {
         //TODO: devolver o dinheiro que não foi gasto
+        defer {
+            self.money = 0
+        }
+        return money
     }
 }
 
+let vendingMachine = VendingMachine(products: [.init(name: "Mainecrafti oliginal", amount: 50, price: 15),
+                                               .init(name: "Milhopã", amount: 100, price: 2),
+                                               .init(name: "Julho", amount: 1, price: 3000),
+                                               .init(name: "Sorvete La Basque", amount: 1, price: 38)])
+
+for _ in 0...10 {
+    let money = Double.random(in: 10...4000)
+    let product = ["Relampago Marquinhos", "MiLhOpÃ", "Julho", "Computador RGB", "mainecrafti oliginal", "Sorvete la bosque", "sorvete La basque"].randomElement()!
+
+    do {
+        print("Quieres comprar un \(product)?")
+        try vendingMachine.getProduct(named: product, with: money)
+        print("Grats, you comprou um \(product), take \(vendingMachine.getTroco()) de volte")
+    } catch let err as VendingMachineError {
+        switch err {
+        case .insufficientFunds:
+            print("No money bro")
+        case .productNotFound:
+            print("No producto hombre")
+        case  .productStuck:
+            print("Instructions unclear, got product stuck and you lost your diñero")
+        case .productUnavailable:
+            print("Product not found #404")
+        }
+    } catch {
+        print("Erro desconhecido")
+    }
+}
